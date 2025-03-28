f6daead8e1d010db961c61b3662e7ccbc2409bf3
kimkaun <s2302@e-mirim.hs.kr>
2025-03-28 10:14:55 +0900

    Read(Get)

--- app2.js
+++ app2.js
@@ -14,8 +14,6 @@ const db = mysql.createConnection({
   database : process.env.DB_NAME
 });
 
-travelList = ['파리', '일본', '스위스', '이탈리아'];
-
 app.set('view engine', 'ejs');
 // __dirname : 현재 파일이 속한 절대경로
 // path.join을 사용하면 운영체제에 맞추어 경로 구분자(/, \)를 알아서 정해준다.
@@ -23,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 db.connect(err => {
   if(err) {
-    console.err('MYSQL 연결 실패 : ', err);
+    console.error('MYSQL 연결 실패 : ', err);
     return;
   }
   console.log('MYSQL에 연결 되었습니다.');
@@ -34,7 +32,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/travel', (req, res) => {
+  const query = 'select id, name from travelList';
+  db.query(query, (err, results) => {
+    if(err) {
+      console.error('데이터베이스 쿼리 실패');
+      resstatus(500).send('Internal Server Error');
+      return;
+    }
+    const travelList = results;
     res.render('travel', {travelList});
+  })
 });
 
 // 서버가 포트 3001에서 요청을 대기합니다.
