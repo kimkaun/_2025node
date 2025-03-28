c3544bda9edda079ba61085d6cae11deb9c87a4a
kimkaun <s2302@e-mirim.hs.kr>
2025-03-28 09:48:39 +0900

    MYSQL 연결

--- app2.js
+++ app2.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
 
 const app = express();
+
+const db = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',    
+  password : 'mirim2',
+  database : 'traveldb'
+});
+
 travelList = ['파리', '일본', '스위스', '이탈리아'];
 
 app.set('view engine', 'ejs');
 // __dirname : 현재 파일이 속한 절대경로
 // path.join을 사용하면 운영체제에 맞추어 경로 구분자(/, \)를 알아서 정해준다.
 app.set('views', path.join(__dirname, 'views'));
+
+db.connect(err => {
+  if(err) {
+    console.err('MYSQL 연결 실패 : ', err);
+    return;
+  }
+  console.log('MYSQL에 연결 되었습니다.');
+});
 
 app.get('/', (req, res) => {
     res.render('home');
