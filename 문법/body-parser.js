ba2178988ed5211cce67c8a299704419943d5220
kimkaun <s2302@e-mirim.hs.kr>
2025-03-31 11:04:26 +0900

    요즘 body-parser

--- 문법/body-parser.js
+++ 문법/body-parser.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 app.post('/submit', (req, res) => {
   const {name, year} = req.body;
