42d1ab021b95d78cdc03d96c04ccaeac8003f5f2
kimkaun <s2302@e-mirim.hs.kr>
2025-04-14 10:56:25 +0900

    라우팅

---
+++ db.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+const mysql = require('mysql2');
+dotenv.config();
+
+// .env로 민감한 데이터를 이동
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect(err => {
+    if(err) {
+        console.err('MySQL 연결 실패:', err);
+        return;
+    }
+    console.log('MySQL에 연결되었습니다.')
+});
+
+module.exports = db;
