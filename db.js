f913b69a82b789fda1146eeb8462d0137e75a860
kimkaun <s2302@e-mirim.hs.kr>
2025-04-21 10:42:07 +0900

    await, async

--- db.js
+++ db.js
@@ -3,11 +3,14 @@ const mysql = require('mysql2');
 dotenv.config();
 
 // .env로 민감한 데이터를 이동
-const db = mysql.createPool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
+// Promise 기반의 쿼리를 사용
+const db = pool.promise();
 
 module.exports = db;
